1c81283d071a85097c9d33208446e65ea6e367c4
Aaron Neiderhiser <aaron.neiderhiser@healthcatalyst.com>
2016-12-19 00:40:28 -0700

    added bisection method to payment calculator

--- credit_card.py
+++ credit_card.py
@@ -10,8 +10,7 @@ def min_payment(balance, r):
         
         month_payment = balance*min_payrate
         total_payments += month_payment 
-#        balance = pay_cycle(balance)
-        
+
         interest = balance*(r/12)
         principal = month_payment - interest
         balance = balance - principal
@@ -25,18 +24,49 @@ def min_payment(balance, r):
     print('Total paid amount: ', "%.2f" % total_payments)
     print('Remaining balance: ', "%.2f" % round(balance,2))
 
+
 ''' Problem 2 - Paying Debt Off in a Year '''
 def payoff_oneyear(balance, r):
-    
-    monthly_rate = r/12.0
 
     for guess in list(range(10,int(balance),10)):
         new_balance = balance
-        while new_balance > 0: ##find number of months to payoff
-
+        num_payments = 0
+        
+        while new_balance > 0 and num_payments < 13: ##find number of months to payoff
+            new_balance = new_balance*(1 + r/12) - guess
+            num_payments += 1
+            
+        if num_payments < 13 and new_balance <= 0:
+            break
+        
     print('RESULT')    
-    print('Monthly payment to pay off debt in 1 year: ', "%.2f" % monthly_payment)
-    print('Number of months needed: ', "%.2f" % num_months)
+    print('Monthly payment to pay off debt in 1 year: ', "%.0f" % guess)
+    print('Number of months needed: ', "%.0f" % num_payments)
+    print('Balance: ', "%.2f" % new_balance)
+    
+    
+''' Problem 3 - Paying Off Debt in a Year with Bisection Search '''
+def bisection(balance, r):
+    low = balance/12
+    high = (balance*(1 + r/12)**12)/12
+    tolerance = .2
+    
+    while abs(balance) > tolerance:
+        guess = (high + low)/2
+        new_balance = balance
+        
+        for i in range(1,13):
+            new_balance = new_balance*(1 + r/12) - guess
+            
+        if new_balance > 0:
+            low = guess
+        else:
+            high = guess
+    
+    print('RESULT')    
+    print('Monthly payment to pay off debt in 1 year: ', "%.0f" % guess)
+    #print('Number of months needed: ', "%.0f" % num_payments)
+    print('Balance: ', "%.2f" % new_balance) 
     
 def main(): 
 
@@ -49,7 +79,7 @@ def main():
     elif program == 2:
         payoff_oneyear(balance, r)
     elif program == 3:
-        ##
+        bisection(balance, r)
     else:
         print('Not a valid program number.')
 
